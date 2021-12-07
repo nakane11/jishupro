c26356317f725cadc2c05f67d445fc47cbc5ba74
nakane11 <nakane-ao@g.ecc.u-tokyo.ac.jp>
2021-12-07 16:00:18 +0900

    update action.c

--- glview/action.c
+++ glview/action.c
@@ -13,41 +13,52 @@ enum action {
     SIT, 
 }
 
-void nextAction(int i) {
-    //stateに応じて次のstate, actionを決める
+//nextActionより周期長くする
+void nextState(int i) {
+    //stateに応じて次のstateを決める
     switch((int)getMat(i, 8)) {
         case ASLEEP:
+        //確率0.15でSTAY
             // setMat(i, 8)=;
             // setMat(i, 9)=;
 
             break;
         case MOVE:
+        //確率0.3でSTAY
             break;
         case STAY:
+        //確率0.2でASLEEP 0.3で
             break;
         default:
             printf("nextAction %d default\n",i);
     }
 
-    //actionに応じて次の位置を決める
-    switch((int)getMat(i, 9)) {
-        case SLEEP:
-            
+}
+
+void nextAction(int i) {
+    
+
+    //stateに応じて次のactionを決める
+    switch((int)getMat(i, 8)) {
+        case ASLEEP:
+        //確率0.15でSTAY
+            // setMat(i, 8)=;
+            // setMat(i, 9)=;
 
             break;
-        case WALK:
+        case MOVE:
+        //確率0.3でSTAY
             break;
-        case RUN:
+        case STAY:
+        //確率0.2でASLEEP 0.3で
             break;
-        case SIT:
-            break;
-        case EAT:
-            break;
-
         default:
             printf("nextAction %d default\n",i);
     }
+    //決まったactionに応じて次の位置を決める
+
 
     
 
 }
+
